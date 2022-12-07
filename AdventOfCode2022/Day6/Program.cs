2008918a93d4a7d58fd5447ec31f7c6d93546bde
Dæmon <xdamon@hotmail.co.uk>
2022-12-07 10:45:40 +0000

    Day 6 completed

---
+++ AdventOfCode2022/Day6/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Day6;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine($"Part 1 Solution: {Solution(4)}");    // 1651
+        Console.WriteLine($"Part 2 Solution: {Solution(14)}");   // 3837
+    }
+
+    public static int Solution(int distinctCharsRequired)
+    {
+        const string file = "Day6Puzzle.txt";
+        var line = File.ReadAllLines(file)[0];
+        for (int i = 0; i + distinctCharsRequired < line.Length; i++)
+        {
+            var curSet = line[i..(i + distinctCharsRequired)].ToHashSet();
+            if (curSet.Count == distinctCharsRequired)
+            {
+                return i + distinctCharsRequired;
+            }
+        }
+        return 0;
+    }
+}
